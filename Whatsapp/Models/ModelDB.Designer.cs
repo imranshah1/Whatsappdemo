147e19a32c51b940debc7d83da43d53dd25bc0c5
imranshah20 <imran_shah20@hotmail.com>
2020-04-18 23:09:57 +0500

    Still working on book

--- Whatsapp/Models/ModelDB.Designer.cs
+++ Whatsapp/Models/ModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Imran Shah\Documents\Visual Studio 2015\Projects\Whatsapp\Whatsapp\Models\ModelDB.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects\Whatsapp\Whatsapp\Models\ModelDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
